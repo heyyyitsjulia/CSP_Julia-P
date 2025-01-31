b601c714374a909985babb91fa0770f2b52bf2db
heyyyitsjulia <julia.properzi@ucas-edu.net>
2025-01-31 11:13:16 -0700

    C variables practice

---
+++ variables/practice.c
@@ -0,0 +1,19 @@
+char name[] = "Julia";
+
+int number = 5;
+
+int big_number = 263;
+
+char breakfast[] = "muffin";
+
+char color[] = "purple";
+
+char school_name[] = "UCAS";
+
+int year = 2025;
+
+char color[] = "blue";
+
+int age = 15;
+
+char fave_subject[] = "Biology";
