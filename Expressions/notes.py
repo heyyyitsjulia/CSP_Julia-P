8b42487f81478e722056bc611d0f4fb19f56fea0
heyyyitsjulia <julia.properzi@ucas-edu.net>
2025-02-04 11:34:41 -0700

    expressions practice

--- Expressions/notes.py
+++ Expressions/notes.py
@@ -8,10 +8,10 @@
 #print(70//8) #integer division returns jus thte whole number
 #print(70%8) #modulo/modulus/MOD return the remainder
 
-num = 4
-print(num)
-num /= 2
-print(num)
+#num = 4
+#print(num)
+#num /= 2
+#print(num)
 
 pi = 3.1415926535
 
