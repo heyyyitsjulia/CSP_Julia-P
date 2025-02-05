7cad0f8aca96e570c0c7b6d12b68505ba5727ea3
heyyyitsjulia <julia.properzi@ucas-edu.net>
2025-02-05 11:35:37 -0700

    expressions practice python

--- Expressions/notes.c
+++ Expressions/notes.c
@@ -5,11 +5,13 @@
 //float equation = 5*7+11/(5-x);
 int num = 12;
 float pi = 3.14159274;
+float expression = 5*pow(7,2)/4;
 // + - * / 
 int main(void){
  //  num = 4;
     //printf("%d\n", num);
     //printf("%f\n", equation);
-    printf("%.10f", pi);
+   // printf ("%f", expression);
+   // printf("%.10f", pi);
     return 0;
 }
