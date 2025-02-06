2ca0eb6d5c90aafafb72aee9bfe11db1341d5790
heyyyitsjulia <julia.properzi@ucas-edu.net>
2025-02-06 11:15:22 -0700

    expressions practice C

--- Expressions/expressionpractice.c
+++ Expressions/expressionpractice.c
@@ -1,7 +1,16 @@
 // Julia Properzi, TEMPLATE
 #include <stdio.h>
 
+float equation = 5*7+11/(5-3);
+int num = 12;
+float pi = 3.14159274;
+float expression = 5*7/4;
+//+ - * / 
 int main(void){
-    printf("Hello World");
+    num = 4;
+    printf("%d\n", num);
+    printf("%f\n", equation);
+    printf ("%f", expression);
+    printf("%.10f", pi);
     return 0;
 }
