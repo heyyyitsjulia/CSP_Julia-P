e67cca1c03d535de8a8bf2727fbcc4c88fe077b1
heyyyitsjulia <julia.properzi@ucas-edu.net>
2025-02-10 11:49:08 -0700

    python notes for string

---
+++ strings/notes.py
@@ -0,0 +1,13 @@
+# Julia Properzi, strings notes python
+
+#string is a data type that holds any info surrounded by quotation marks "" ''
+
+note = "Vienna's class"
+
+name = (input("What is your first name?\n").strip().capitalize() )
+
+#print(f"Hello {name} wlcome to my program")
+print("This is your name"+ name)
+
+sentence = "The quick brown fox jumps over the lazy dog"
+
