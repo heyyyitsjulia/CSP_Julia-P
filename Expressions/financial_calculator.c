b4b4d3199db58fce7ad93ccede4789a2ff0723f5
heyyyitsjulia <julia.properzi@ucas-edu.net>
2025-02-13 11:49:04 -0700

    financial calculator C

--- Expressions/financial_calculator.c
+++ Expressions/financial_calculator.c
@@ -1,7 +1,25 @@
 // Julia Properzi, Financial Calculator C
 #include <stdio.h>
 
+float income;
+float rent;
+float utilities;
+float groceries;
+float transportation;
+float savings;
+
 int main(void){
-    printf("Hello World");
+    printf("Welcome to my program this will create a personal finance calculator and will help you figure out how much you need to put into\nsavings, based off your income and expenses!\n");
+    printf("What is your monthly income (don't use commas): \n");
+    scanf("%f", &income);
+    printf("What is your monthly rent?\n");
+    scanf("%f", &rent);
+    printf("What is the cost of your utilities? every month\n");
+    scanf("%f", &utilities);
+    printf("What is the average cost of your groceries?\n");
+    scanf("%f", &groceries);
+    printf("How much does it cost for your transportation?\n");
+    scanf("%f", &transportation);
+    printf(&income*.1);
     return 0;
 }
--- strings/silly_sentences.c
+++ strings/silly_sentences.c
@@ -1,7 +1,13 @@
 // Julia Properzi, silly sentences C
 #include <stdio.h>
+// empty string variables for user words (minimun 3)
 
 int main(void){
-    printf("Hello World");
+    //A welcome for the user telling them what the program is (print)
+
+    //ask user for words (print statement with a question scanf to set to variable) (in C we need to tell the user that they only type 1 word)
+
+    //print out the story with the variables inserted ("welcom %s to my program", name)
+
     return 0;
 }
