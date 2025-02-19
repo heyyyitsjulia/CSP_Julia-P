6abae688011174786fdc18d501e8e2b8a84676ed
heyyyitsjulia <julia.properzi@ucas-edu.net>
2025-02-19 11:31:58 -0700

    first program C

--- variables/firstprogram.c
+++ variables/firstprogram.c
@@ -6,7 +6,6 @@ char color[] = "purple";
 
 int main(void){
     printf("Hello i am %s. I am %d years old. I like the color %s\n", name, age, color);
-    printf("%d\n", age);
-    printf("%s\n", color);
+
     return 0;
 }
