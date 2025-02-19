f94b317979ba726af9a4e0d7af5978db0ef04003
heyyyitsjulia <julia.properzi@ucas-edu.net>
2025-02-19 11:29:23 -0700

    variables practice C

--- Functions/notes.c
+++ Functions/notes.c
@@ -5,11 +5,15 @@ char name[50], place[50], verb[50];
 int add(int numOne, int numTwo){
     return "%d\n", numOne+ numTwo;
 }
-const char* word(char type[50]){
-    char choice[50];
-    printf("Please give me a %s:\n",type);
-    scanf("%s", choice);
-    return choice;
+//const char* word(char type[50]){
+    //char choice[50];
+    //printf("Please give me a %s:\n",type);
+    //scanf("%s", choice);
+    //return choice;
+
+
+void due(char assignment[50], char day[20]){
+    printf("The %s assignment is due %s", assignment, day);
 }
 
 int main(void){
@@ -17,6 +21,9 @@ int main(void){
    // scanf("%d", num);
    // add(num,10);
     //add(8,1);
-    printf("%d",add(72,5));
+    //printf("%d",add(72,5));
+    due("Funcions Notes", "Today");
+    due("Hello World Update", "Tomorrow");
+    due("Financial calculator Update", "Friday");
     return 0;
 }
--- variables/practice.c
+++ variables/practice.c
@@ -1,19 +1,26 @@
-char name[] = "Julia";
-printf(name)
-int number= 5;
-printf(number)
-int big_number = 263;
+// Julia Properzi, Variables practice
+#include<stdio.h>
 
+int main(void){
+    char name[] = "Julia";
+printf("%s\n", name);
+char number[] = "5";
+printf("%s\n", number);
+char bigNumber[] = "263";
+printf("%s\n", bigNumber);
 char breakfast[] = "muffin";
-
+printf("%s\n", breakfast);
 char color[] = "purple";
-
+printf("%s\n", color);
 char school_name[] = "UCAS";
-
-int year = 2025;
-
-char color[] = "blue";
-
-int age = 15;
-
+printf("%s\n", school_name);
+char year[] = "2025";
+printf("%s\n", year);
+char eye_color[] = "blue";
+printf("%s\n", eye_color);
+char age []= "15";
+printf("%s\n", age);
 char fave_subject[] = "Biology";
+printf("%s\n", fave_subject);
+return 0;
+}
