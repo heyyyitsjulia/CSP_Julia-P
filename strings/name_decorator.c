961c237d57773e5cefed5693e665f53dae3d8939
heyyyitsjulia <julia.properzi@ucas-edu.net>
2025-02-19 19:36:19 +0000

    name decorator C

--- strings/name_decorator.c
+++ strings/name_decorator.c
@@ -1,8 +1,12 @@
 // Julia Properzi, Name Decorator C
 #include <stdio.h>
+#include<string.h>
 
 int main(void){
-    char name = (input("What is your first name?\n"));
-    printf("%s", name);
+    char name[30] = "Julia";
+    char capSix[40] = "<<<<";
+    strcat(name, ">>>>")
+    strcat(capSix, name);
+    printf("Hello %s \n", capSix);
     return 0;
 }
