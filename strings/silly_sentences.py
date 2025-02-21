75b8b5012970b839ae18f4216dee1736860c3ab5
heyyyitsjulia <julia.properzi@ucas-edu.net>
2025-02-21 11:35:23 -0700

    silly sentences python

--- strings/silly_sentences.py
+++ strings/silly_sentences.py
@@ -1,12 +1,11 @@
 # Julia Properzi, silly sentences C
 
 # empty string variables for user words (minimun 3)
-adjective =input("Tell me a adjective\n")
-verb = input("Tell me a verb\n")
+adjective =input("Tell me a adjective:\n")
+celebrity = input("Tell me a celebrity:\n")
+verb = input("Tell me a verb(ing):\n")
+place = input("Tell me a place:\n")
+emotion = input("Tell me an emotion:\n")
 
-    #A welcome for the user telling them what the program is (print)
-
-    #ask user for words (print statement with a question scanf to set to variable) (in C we need to tell the user that they only type 1 word)
-
-    #print out the story with the variables inserted ("welcom %s to my program", name)
-
+print(f"It was a {adjective} day when I saw {celebrity} {verb} all the way to {place}. When I saw this I was very {emotion}.\n I could not believe I just witnessed that.")
+   
