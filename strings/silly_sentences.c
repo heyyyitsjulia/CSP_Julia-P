022998f81dd1fb2f2d0f0a90515c21a8bf0a5856
heyyyitsjulia <julia.properzi@ucas-edu.net>
2025-02-21 11:48:50 -0700

    silly sentences C

--- strings/silly_sentences.c
+++ strings/silly_sentences.c
@@ -1,13 +1,26 @@
 // Julia Properzi, silly sentences C
 #include <stdio.h>
 // empty string variables for user words (minimun 3)
-
+char adjective [20];
+char celebrity[20];
+char verb[20];
+char place[20];
+char emotion[20];
 int main(void){
-    //A welcome for the user telling them what the program is (print)
+    
+    printf("Tell me an adjective:\n");
+    scanf("%s", adjective);
+    printf("Tell me a celebrity:\n");
+    scanf("%s", celebrity);
+    printf("Tell me a verb: \n");
+    scanf("%s", verb);
+    printf("Tell me a place: \n");
+    scanf("%s", place);
+    printf("Tell me an emotion:\n");
+    scanf("%s", emotion);
 
-    //ask user for words (print statement with a question scanf to set to variable) (in C we need to tell the user that they only type 1 word)
+    printf("It was a %s day when I saw %s %s all the way to %s. When I saw this I was very %s. I could not believe I just witnessed that.", adjective, celebrity, verb, place, emotion);
 
-    //print out the story with the variables inserted ("welcom %s to my program", name)
 
     return 0;
 }
