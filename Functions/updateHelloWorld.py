92acae6a3cd7b709bec0b4d18446b81aca417f1c
heyyyitsjulia <julia.properzi@ucas-edu.net>
2025-02-28 11:18:57 -0700

    update hello world python

---
+++ Functions/updateHelloWorld.py
@@ -0,0 +1,12 @@
+# Julia Properzi, update hello world
+
+fname = input("What is your first name:\n")
+print(f"hello {fname}!")
+tname = input("What is your teachers name:\n")
+print(f"hello {tname}!")
+friname = input("what is your friends name:\n")
+print(f"hello {friname}!")
+momName = input("what is your mom's name:\n")
+print(f"hello {momName}!")
+dadName = input("what is your dad's name:\n")
+print(f"hello {dadName}!")
