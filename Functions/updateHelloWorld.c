a16bf1b3f27357bdcb5669c4c74547dfbb64d28c
heyyyitsjulia <julia.properzi@ucas-edu.net>
2025-02-28 11:49:33 -0700

    update hello world C

--- Functions/updateHelloWorld.c
+++ Functions/updateHelloWorld.c
@@ -1,16 +1,28 @@
 // Julia Properzi, update hello world
 #include <stdio.h>
 
-char name [20];
-char tname [20];
-char fname [20];
-char momName [20];
-char dadName [20];
 
-int main(void){
-    printf("What is your name:\n");
+//tname[50], fname[50], momName[50], dadName[50];
+
+ void word(char name){
+    printf("hello %s", name); 
+    char name[50];
+    //char tname[50];
+    //char fname[50];
+    //char momName[50];
+    //char dadName[50];
+
+    printf("Please tell me your name:\n");
     scanf("%s", name);
-    printf("Hello {name})
-    printf("What is your teachers name:\n");
+    hello(name);
     return 0;
 }
+
+
+//int main(void){
+    //printf("What is your name:\n");
+    //scanf("%s", name);
+    //printf("Hello {name})
+    //printf("What is your teachers name:\n");
+    //return 0;
+//}
