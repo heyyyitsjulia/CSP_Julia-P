41b3e6631b32884085abb864c7569c2f7a617e44
heyyyitsjulia <julia.properzi@ucas-edu.net>
2025-02-28 22:37:19 +0000

    update hello world c

--- Functions/updateHelloWorld.c
+++ Functions/updateHelloWorld.c
@@ -5,24 +5,37 @@
 //tname[50], fname[50], momName[50], dadName[50];
 
  void word(char name){
-    printf("hello %s", name); 
     char name[50];
-    //char tname[50];
-    //char fname[50];
-    //char momName[50];
-    //char dadName[50];
+    printf("hello %s", name); 
+    char tname[50];
+    printf("hello %s", tname);
+    char fname[50];
+    printf("hello %s", fname);
+    char momName[50];
+    printf("hello %s", momName);
+    char dadName[50];
+    printf("hello %s", dadName);
 
     printf("Please tell me your name:\n");
     scanf("%s", name);
     hello(name);
+
+    printf("Tell me your teachers name:\n");
+    scanf("%s", tname);
+    hello(tname);
+
+    printf("What is one of your friends name:\n");
+    scanf("%s", fname);
+    hello(fname);
+
+    printf("What is your mom's name:\n");
+    scanf("%s", momName);
+    hello(momName);
+
+    printf("what is your dad's name:\n");
+    scanf("%s", dadName);
+    hello(dadName);
+
+
     return 0;
 }
-
-
-//int main(void){
-    //printf("What is your name:\n");
-    //scanf("%s", name);
-    //printf("Hello {name})
-    //printf("What is your teachers name:\n");
-    //return 0;
-//}
