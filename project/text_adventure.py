7426c75466e46ad82f686e1a28f7989eb5a509cd
heyyyitsjulia <julia.properzi@ucas-edu.net>
2025-03-25 11:49:17 -0600

    final project
    Co-authored-by: Elizabeth150-9 <Elizabeth150-9@users.noreply.github.com>
    Co-authored-by: jolleypaige <jolleypaige@users.noreply.github.com>
    Co-authored-by: TEDDYBEARS11 <TEDDYBEARS11@users.noreply.github.com>

--- project/text_adventure.py
+++ project/text_adventure.py
@@ -1,32 +1,42 @@
 #Text based adventure game, python
-x = incorrect
 print("Hello player, welcome to our text based adventure game! Pick 1 of the each of the 2 puzzles to solve.")
 
-print("choose your number. 1 or 2")
-if 1
-    print ("guess the object based on the description. summer, red, fruit, sliced.\n")
-elif 
-    print ("watermelon\n")
-elif
-    print
-else:
-    print("do it again.\n")
+question = input ("choose a number! 1 or 2!\n")
+1 == ("Find the word based on the description!\n")
+2 ==("finish the sequence!\n")
+if question == 1:
+    print("156723451567234515672\n")
+    question ==("345")
+elif question == 2:
+    print(" Red, summer, fruit, sliced.\n")
+    input == ("watermelon")
+hai = print(input("you have white granny hair!"))
+hair = print(input("you have lilac hair!"))
 
-riddle_answer = input ("Here is your riddle, What is always on the floor, but never gets dirty:\n")
-if riddle_answer == "shadow":
-    print(f"You are correct! {riddle_answer} was the answer.")
-else:
-    input(f"That is incorrect, try again!\n")
+secondquestion = input ("")
+while True:  
+    riddle_answer = input("Here is your riddle, What is always on the floor, but never gets dirty:\n")  
+    if riddle_answer == "shadow":  
+        print("You are correct!\n You got a big blue ball gown with elegant puffed sleeves.")  
+        break   
+    else:  
+        print("That is incorrect, try again:")  
 
-puzzle = input("Pick 1 of these 2 puzzles options.\n")
-3 = ("Word scramble!\n")
-4 = ("Another riddle!\n")
+puzzle = input("To pick 1 a word scramble or riddle,  insert 3 or 4.\n")
+third = ("imklerci\n")
+fourth = input("Maria has five children. Jack, Logan, Sarah, and Lipton. What is the name of the fifth child?\n")
 if puzzle == 3:
-    print("rkimceli\n")
-elif puzzle == 4:
-    print("\n")
+    print("imklerci\n")
+elif third == "Limerick":
+    print("Correct, you won black eyes!\n")
+else:
+    print("Incorrect, try again...\n")
+if puzzle == 4:
+    print("Maria has five children. Jack, Logan, Sarah, and Lipton. What is the name of the fifth child?\n")
+elif fourth == "What":
+    print("Correct, you won green eyes!\n")
+else:
+    print("Incorrect, try again...\n")
 iyes = int(input("You have black eyes!\n"))
 eyes = int(input("You have green eyes!\n"))
-
-sequence = input("What is the next number in the following sequence? one, three, four, five, nine, eight, seventeen, three, ")
 
--- project/text_adventureJULIA.py
+++ project/text_adventureJULIA.py
@@ -1,5 +1,14 @@
-riddle_answer = input ("Here is your riddle, What is always on the floor, but never gets dirty:\n")
-if riddle_answer == "shadow":
-    print(f"You are correct! {riddle_answer}was the answer.")
-else:
-    input(f"That is incorrect, try again!\n")
+#riddle_answer = input ("Here is your riddle, What is always on the floor, but never gets dirty:\n")
+#if riddle_answer == "shadow":
+    #print(f"You are correct! {riddle_answer}was the answer.")
+#else:
+    #input(f"That is incorrect, try again!\n")
+#while riddle_answer == "shadow":
+
+while True:  
+    riddle_answer = input("Here is your riddle, What is always on the floor, but never gets dirty:\n")  
+    if riddle_answer == "shadow":  
+        print("You are correct!\n You got a big blue ball gown with elegant puffed sleeves.")  
+        break  # This exits the loop  
+    else:  
+        print("That is incorrect, try again:")  
