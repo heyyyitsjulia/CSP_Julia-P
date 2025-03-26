6c27a36fcc14d7f7361c5b3213840100390412a4
heyyyitsjulia <julia.properzi@ucas-edu.net>
2025-03-26 11:20:59 -0600

    final project
    Co-authored-by: Elizabeth150-9 <Elizabeth150-9@users.noreply.github.com>
    Co-authored-by: jolleypaige <jolleypaige@users.noreply.github.com>
    Co-authored-by: TEDDYBEARS11 <TEDDYBEARS11@users.noreply.github.com>

--- project/text_adventure.py
+++ project/text_adventure.py
@@ -1,19 +1,22 @@
 #Text based adventure game, python
 print("Hello player, welcome to our text based adventure game! Pick 1 of the each of the 2 puzzles to solve.")
-
+ 
 question = input ("choose a number! 1 or 2!\n")
-1 == ("Find the word based on the description!\n")
-2 ==("finish the sequence!\n")
 if question == 1:
+    print("finish the sequence!\n")
     print("156723451567234515672\n")
-    question ==("345")
-elif question == 2:
-    print(" Red, summer, fruit, sliced.\n")
+if  question == ("156723451567234515672\n"):
+    input == ("345") 
+    print("you are correct!\n You get granny white hair.")
+else:
+    print ("Find the word based on the description!")
+    question == (" Red, summer, fruit, sliced.\n")
+
     input == ("watermelon")
 hai = print(input("you have white granny hair!"))
 hair = print(input("you have lilac hair!"))
 
-secondquestion = input ("")
+secondquestion = input ("pick a number, 7 or 8")
 while True:  
     riddle_answer = input("Here is your riddle, What is always on the floor, but never gets dirty:\n")  
     if riddle_answer == "shadow":  
@@ -22,21 +25,22 @@ while True:
     else:  
         print("That is incorrect, try again:")  
 
-puzzle = input("To pick 1 a word scramble or riddle,  insert 3 or 4.\n")
-third = ("imklerci\n")
-fourth = input("Maria has five children. Jack, Logan, Sarah, and Lipton. What is the name of the fifth child?\n")
-if puzzle == 3:
-    print("imklerci\n")
-elif third == "Limerick":
-    print("Correct, you won black eyes!\n")
-else:
-    print("Incorrect, try again...\n")
-if puzzle == 4:
-    print("Maria has five children. Jack, Logan, Sarah, and Lipton. What is the name of the fifth child?\n")
-elif fourth == "What":
-    print("Correct, you won green eyes!\n")
-else:
-    print("Incorrect, try again...\n")
-iyes = int(input("You have black eyes!\n"))
-eyes = int(input("You have green eyes!\n"))
+while True:    
+    puzzle = input("To pick 1 a word scramble or riddle, insert 3 or 4.\n")    
+    if puzzle == "3":   
+        answer = input("imekilcr\n")   
+        if answer.lower() == "limerick":
+            print("Correct, you won black eyes!\n") 
+            break  
+        else:
+            print("Incorrect, try again...\n")
+    elif puzzle == "4":  
+        answer = input("Maria has five children. Jack, Logan, Sarah, and Lipton. What is the name of the fifth child?\n")    
+        if answer.lower() == "what":    
+            print("Correct, you won green eyes!\n")    
+            break   
+        else:    
+            print("Incorrect, try again...\n")    
+    else:  
+        print("To pick 1 a word scramble or riddle, insert 3 or 4.\n")  
 
--- project/text_adventureJULIA.py
+++ project/text_adventureJULIA.py
@@ -4,11 +4,14 @@
 #else:
     #input(f"That is incorrect, try again!\n")
 #while riddle_answer == "shadow":
-
+secondquestion = input ("pick a number, 7 or 8")
 while True:  
-    riddle_answer = input("Here is your riddle, What is always on the floor, but never gets dirty:\n")  
+    if 7 == secondquestion:
+        riddle_answer = input("Here is your riddle, What is always on the floor, but never gets dirty:\n")  
     if riddle_answer == "shadow":  
         print("You are correct!\n You got a big blue ball gown with elegant puffed sleeves.")  
-        break  # This exits the loop  
+        break 
     else:  
         print("That is incorrect, try again:")  
+    if 8 == secondquestion: 
+        unscramble = input("Unscramble this word... ")
