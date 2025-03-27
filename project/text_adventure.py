8042d41de6318cffa0e8c33d0f81c79519b25302
heyyyitsjulia <julia.properzi@ucas-edu.net>
2025-03-27 11:20:01 -0600

    final
    Co-authored-by: Elizabeth150-9 <Elizabeth150-9@users.noreply.github.com>
    Co-authored-by: TEDDYBEARS11 <TEDDYBEARS11@users.noreply.github.com>
    Co-authored-by: jolleypaige <jolleypaige@users.noreply.github.com>

--- project/text_adventure.py
+++ project/text_adventure.py
@@ -1,16 +1,43 @@
 #Text based adventure game, python
 print("Hello player, welcome to our text based adventure game! Pick 1 of the each of the 2 puzzles to solve.")
-while True:  
-    question = input("choose a number! 1 or 2!\n")  
-    if question == "1":  
-        # your code for question "1"  
-    elif question == "2":  
-        # your code for question "2"  
-while True:
-question = input ("choose a number! 1 or 2!\n")
-    if question is = 1 or sequence = 2
-        if 1 == question:
-        question = input("Finish the sequence! 156723451567234515672\n")
+if secondquestion == 7:    
+    while True:    
+        riddle_answer = input("Here is your riddle, What is always on the floor, but never gets dirty:\n")    
+        if riddle_answer == "shadow":    
+            print("You are correct!\n You got a big blue ball gown with elegant puffed sleeves.")    
+            break   
+        else:    
+            print("That is incorrect, try again:")  
+elif secondquestion == 8:   
+    while True:  
+        unscramble = input("Unscramble this word... stcilvaiiznoi:\n")  
+        if unscramble == "civilizations":  
+            print("You are correct! Wow, you're good at this! You got jean overalls with embroidered flowers on the pockets.")  
+            break  
+        else:  
+            print("That is WRONG, try again:")  
+else:  
+    print("That is not a valid option. Please pick 7 or 8.")  
+
+
+
+
+
+
+puzzleone = int(input("pick a number, 1 or 2\n"))
+if puzzleone == 1:
+    while True:  
+question = input("guess the qord based on the description! red, summer,fruit, sliced:\n")  
+if question == "watermelon":
+print("you are correct!\n You get granny white hair!")
+break
+    else:
+print("that is incorrect, try again:")
+ elif questiontwo == 2:  
+ while Tfinishtiontwo = input("Finish the sequence! 156723451567234515672:\n")if finish == "345":
+print("you are correct! You get light up neon circus afro hair!")
+     
+  
     if question == "345":
     print(f"that is correct! {question} is correct! You get granny white hair!" )
         break 
@@ -47,6 +74,27 @@ while True:
 
 
 
+sequence = int(input("Pick either number 5 or 6 for your next puzzle.\n"))
+if sequence == 5:
+  while True:
+    sequence_puzzle = input("What is the next number in the sequence? one, three, four, five, nine, two, eleven, four,...")
+    if sequence_puzzle =="fifteen":
+     print(f"That's correct! {sequence_puzzle} is the answer. You are wearing purple heelys. ")
+     break
+    else:
+      print("That is incorrect, try again.")
+elif sequence == 6:
+  while True:
+    sequence_puzzle_two = input("What is the next letter in the sequence? a, d, h, k, o, r,...")
+    if sequence_puzzle_two =="u":
+     print(f"That's correct! {sequence_puzzle_two} is the answer. You are wearing pink bedazzeled justice glow up sneakers! ")
+     break
+    else:
+      print("That is incorrect, try again.")
+        
+
+
+
 secondquestion = int(input("pick a number, 7 or 8\n"))    
   
 if secondquestion == 7:    
@@ -61,7 +109,7 @@ elif secondquestion == 8:
     while True:  
         unscramble = input("Unscramble this word... stcilvaiiznoi:\n")  
         if unscramble == "civilizations":  
-            print("You are correct! Wow, you're good at this! You got overalls with embroidered flowers on the pockets.")  
+            print("You are correct! Wow, you're good at this! You got jean overalls with embroidered flowers on the pockets.")  
             break  
         else:  
             print("That is WRONG, try again:")  
