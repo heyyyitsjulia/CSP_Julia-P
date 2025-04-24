de7623f545b94b5c93382836c6f1c17aa3d0cabd
heyyyitsjulia <julia.properzi@ucas-edu.net>
2025-04-24 11:43:54 -0600

    web page

--- javascript/notes.js
+++ javascript/notes.js
@@ -28,3 +28,13 @@ function pop(){
 function show(){
     document.getElementById("lost").style.display = "block"
 }
+function view(){
+    if(document.getElementById("more").style.display ==="block"){
+        document.getElementById("more").style.display ==="none"
+        document.getElementById("shw").innerHTML="Show More"
+    }else{
+        document.getElementById("more").style.display ==="block"
+    }
+    document.getElementById("more").style.display = "block"
+     document.getElementById("shw").innerHTML="Show Less"
+}
