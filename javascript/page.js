06bc71085680f3ed425a21af25b22354384811ab
heyyyitsjulia <julia.properzi@ucas-edu.net>
2025-04-28 11:33:45 -0600

    hidden image

--- javascript/page.js
+++ javascript/page.js
@@ -1,9 +1,15 @@
+let images = ["https://www.wwf.org.uk/sites/default/files/styles/content_slide_image/public/2020-08/Small_WW22392.jpg?h=0e753701&itok=lCMX5Gse"]
+let counter = 0
+
 function change(){
     if (counter < images.length){
         document.getElementById("img").src = images[counter]
         counter += 1
     }else{
         counter = 0 
-        document.getElementById("img").src = images[counter] 
+        document.getElementById("img").style.display ==="none"
     }
+}
+function hover(){
+    document.getElementById("img").style.display=== "none"
 }
