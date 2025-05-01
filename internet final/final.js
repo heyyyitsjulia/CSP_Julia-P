688a6d4d0ac3e20c0942e4bcb0796df77f617de8
heyyyitsjulia <julia.properzi@ucas-edu.net>
2025-05-01 13:26:15 -0600

    final

---
+++ internet final/final.js
@@ -0,0 +1,13 @@
+let images = [ "https://www.datocms-assets.com/19726/1716313326-adobestock_445877528.jpeg", "https://www.hyattvacationclub.com/files/live/sites/hvc-marketing/files/marketing/travel-inspiration/outdoor-adventures/24-503-3181201_WOH_HyattPlaceParkCity_Park_City_Utah_Fall_downtownHVCGettyImages-1277639781_16_9.webp"]
+let counter = 0
+
+function change(){
+    if (counter < images.length){
+        document.getElementById("img 1").src = images[counter]
+        counter += 1
+    }else{
+        counter = 0 
+        document.getElementById("img 1").src = images[counter] 
+    }
+    
+}
