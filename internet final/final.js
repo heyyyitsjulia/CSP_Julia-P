c71923edd6b031b9757f8a351567ed405b2f4d29
heyyyitsjulia <julia.properzi@ucas-edu.net>
2025-05-05 16:29:53 -0600

    FINISHED FINALLLL

--- internet final/final.js
+++ internet final/final.js
@@ -13,10 +13,10 @@ function change(){
 }
 function show(){
     if(document.getElementById("hidden").style.display==="block"){
-        document.getElementById("hidden").style.display="block"
-        document.getElementById("button").innerHTML="show less"
+        document.getElementById("hidden").style.display="none"
+        document.getElementById("button").innerHTML="Show More!"
     }else{
-        document.getElementById("hidden").style.display==="none"
-         document.getElementById("button").innerHTML="show more"
+        document.getElementById("hidden").style.display="block"
+         document.getElementById("button").innerHTML="show less..."
     }
 }
